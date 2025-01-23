156f02b4a8c24ffffeaeb23cf76c19591c26ab34
Tahsin Zilani <safasadaf3@gmail.com>
2025-01-23 16:14:36 +0600

    fix

--- src/App.js
+++ src/App.js
@@ -7,7 +7,18 @@ import Content from "./components/Content";
 const App = () => (
   <Router>
     <Routes>
-      <Route path="/trending" element={<Trending />} />
+      <Route
+        path="/trending"
+        element={
+          <div>
+            <h1>Trending Content</h1>
+            <ul>
+              <li>Movie 1: Description 1</li>
+              <li>Movie 2: Description 2</li>
+            </ul>
+          </div>
+        }
+      />
       <Route path="/content/:id" element={<Content />} />
     </Routes>
   </Router>
