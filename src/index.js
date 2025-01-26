d872be0d41e01a044e1150c58c9808ebd239108e
Yeasin Zilani <sadafzillani98@gmail.com>
2025-01-26 14:27:09 +0600

    fix

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
 // src/index.js
 export { BingeSDK } from "./sdk/BingeSDK";
 export { createTrendingComponent } from "./sdk/BingeSDK";
-export { Trending } from "./components/Trending";
--- src/sdk/BingeSDK.js
+++ src/sdk/BingeSDK.js
@@ -1,15 +1,29 @@
 // src/sdk/BingeSDK.js
+
+import Trending from "../components/Trending";
+
 // src/sdk/BingeSDK.js
 export class BingeSDK {
   async getTrendingContent() {
     // Mock API response
     const data = [
-      { id: 1, title: 'Trending Item 1', description: 'This is a trending item' },
-      { id: 2, title: 'Trending Item 2', description: 'This is another trending item' },
-      { id: 3, title: 'Trending Item 3', description: 'This is yet another trending item' },
+      {
+        id: 1,
+        title: "Trending Item 1",
+        description: "This is a trending item",
+      },
+      {
+        id: 2,
+        title: "Trending Item 2",
+        description: "This is another trending item",
+      },
+      {
+        id: 3,
+        title: "Trending Item 3",
+        description: "This is yet another trending item",
+      },
     ];
 
-    
     await new Promise((resolve) => setTimeout(resolve, 1000));
 
     return data;
@@ -19,6 +33,7 @@ export class BingeSDK {
 export function createTrendingComponent(sdk) {
   return () => {
     const data = sdk.getTrendingContent();
+    console.log(data);
     return <Trending data={data} />;
   };
 }
